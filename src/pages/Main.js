975cdeaa13bd15dd4b4cf64c037714826f1106b9
tobiaserich <tobiaserich@hotmail.de>
2020-04-19 18:46:46 +0200

    Redesign the mainPage structure and add new props

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,16 +6,43 @@ const Header = styled("div")`
   height: 5%;
 `;
 export default function Main() {
+  const [points, setPoints] = React.useState([
+    { name: "AF", points: "0" },
+    { name: "SPO2", points: "0" },
+    { name: "O2 Gabe?", points: "0" },
+    { name: "RR syst", points: "0" },
+    { name: "Puls", points: "0" },
+    { name: "Vigilanz", points: "0" },
+    { name: "Temp", points: "0" },
+  ]);
   return (
     <>
       <Header />
-      <InputBox name="AF"></InputBox>
-      <InputBox name="SpO2"></InputBox>
-      <InputBox name="O2 Gabe?"></InputBox>
-      <InputBox name="RR"></InputBox>
-      <InputBox name="Puls"></InputBox>
-      <InputBox name="Vigilanz"></InputBox>
-      <InputBox name="Temp"></InputBox>
+      {points.map((field, index) => {
+        const description =
+          field.name === "O2 Gabe?"
+            ? {
+                left: { first: "ja", second: "" },
+                right: { first: "nein", second: "" },
+              }
+            : field.name === "Vigilanz"
+            ? {
+                left: { first: "wach", second: "orientiert" },
+                right: { first: "bewusstlos", second: "verwirrt" },
+              }
+            : "";
+
+        return (
+          <InputBox
+            key={field.name}
+            name={field.name}
+            descr={description}
+            points={points}
+            handlePoints={setPoints}
+            position={index}
+          />
+        );
+      })}
     </>
   );
 }
