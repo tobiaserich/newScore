ec339bd76d71f4f32897c7de0b675147836528d1
tobiaserich <tobiaserich@hotmail.de>
2020-05-01 22:08:30 +0200

    Outsource components and rename props for better naming, replace useless functions

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,7 +4,8 @@ import InputBox from "../components/InputBox";
 import { Button, InfoButton } from "../components/Button";
 import Modal from "../components/Modal";
 import Result from "../components/Result";
-import { mqh, mqw } from "../assets/mediquery";
+import { mqh } from "../assets/mediquery";
+
 const Header = styled("div")`
   height: 10px;
 
@@ -14,20 +15,15 @@ const Header = styled("div")`
 `;
 
 const Footer = styled("footer")`
-  height: 35px;
   position: absolute;
   bottom: 0;
-  ${mqw("small")} {
-    height: 45px;
-  }
 `;
 
 export default function Main() {
-  const [resetValue, setResetValue] = React.useState(false);
+  const [reset, setReset] = React.useState(false);
   const [infoActive, setInfoActive] = React.useState(false);
   const [focus, setFocus] = React.useState(0);
-
-  const [points, setPoints] = React.useState([
+  const [formFields, setFormFields] = React.useState([
     { name: "AF", points: 0 },
     { name: "SPO2", points: 0 },
     { name: "O2 Gabe?", points: 0 },
@@ -36,25 +32,26 @@ export default function Main() {
     { name: "Vigilanz", points: 0 },
     { name: "Temp", points: 0 },
   ]);
-
   const infoModal = infoActive ? <Modal onClick={setInfoActive} /> : "";
-  function handleKeyPress(event, position) {
+
+  function handleInputPress(event, position) {
     if (event === "Mouse") {
       setFocus(position);
     } else {
-      if (event.key === "ArrowUp" && focus >= 0) {
+      if (event.key === "ArrowUp" && focus > 0) {
         setFocus(focus - 1);
         console.log(focus);
-      } else if (event.key === "ArrowDown" && focus <= 7) {
+      } else if (event.key === "ArrowDown" && focus < 6) {
         setFocus(focus + 1);
       }
     }
   }
+
   return (
     <>
-      <Header></Header>
+      <Header />
       <form>
-        {points.map((field, index) => {
+        {formFields.map((field, index) => {
           const description =
             field.name === "O2 Gabe?"
               ? {
@@ -70,26 +67,26 @@ export default function Main() {
 
           return (
             <InputBox
-              onKeyUp={handleKeyPress}
+              onInput={handleInputPress}
               focus={focus === index ? true : false}
               key={field.name}
               name={field.name}
               descr={description}
-              points={points}
-              handlePoints={setPoints}
+              fields={formFields}
+              handlePoints={setFormFields}
               position={index}
-              reset={resetValue}
+              reset={reset}
             />
           );
         })}
       </form>
-      <Button onClick={() => setResetValue(!resetValue)}>reset all</Button>
+      <Button onClick={() => setReset(!reset)}>reset all</Button>
 
-      <Result points={points} />
+      <Result points={formFields} />
+      {infoModal}
       <Footer>
         <InfoButton onClick={() => setInfoActive(true)}>?</InfoButton>
       </Footer>
-      {infoModal}
     </>
   );
 }
